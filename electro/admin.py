0a33cae0efa94cb49895a0abb7eaba390d1167cb
Jasmin <jasminnurmanova@webster.edu>
2025-12-05 13:43:24 +0500

    news

--- electro/admin.py
+++ electro/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CarCategory, Car
 
-# Register your models here.
+admin.site.register(CarCategory)
+admin.site.register(Car)
--- electro/models.py
+++ electro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class CarCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(CarCategory, on_delete=models.CASCADE, related_name='cars')
+    brand = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='cars_images/', blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.brand}, {self.year})"
